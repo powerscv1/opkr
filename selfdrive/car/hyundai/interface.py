61cb2693bad77d1db9abd1f6be25988ff494b662
powerscv1 <79405674+powerscv1@users.noreply.github.com>
2022-12-22 16:22:19 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -73,7 +73,7 @@ class CarInterface(CarInterfaceBase):
 
     ret.stoppingControl = False
     ret.vEgoStopping = 0.4  # 1.0, 0.5
-    ret.vEgoStarting = 0.5  # needs to be >= vEgoStopping to avoid state transition oscillation
+    ret.vEgoStarting = 0.3  # needs to be >= vEgoStopping to avoid state transition oscillation
     ret.stopAccel = -1.0 # 0.0, -0.5    
     ret.stoppingDecelRate = 0.7 # 0.8, 0.2  # brake_travel/s while trying to stop
     
